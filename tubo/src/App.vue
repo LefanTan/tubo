6962105f299ebad139bcac9d7c6ec3c9d78ef414
Lefan <lefantan@hotmail.com>
2023-07-04 22:29:04 -0600

    Add logout method

--- tubo/src/App.vue
+++ tubo/src/App.vue
@@ -4,6 +4,10 @@ import useStore from './stores'
 
 const $store = useStore()
 const routes = useRoute()
+
+function onLogout() {
+  $store.user.logout()
+}
 </script>
 
 <template>
@@ -15,12 +19,21 @@ const routes = useRoute()
         <router-link v-if="routes.meta.startNowBtn" to="/login" class="btn px-8 py-3 text-2xl"
           >start now</router-link
         >
-        <q-img
-          v-if="routes.meta.requiresAuth"
-          :src="$store.user.spotifyUser?.images?.[0].url"
-          class="w-14 h-14 rounded-full"
-          alt="profile image"
-        />
+        <button v-if="routes.meta.requiresAuth">
+          <q-img
+            :src="$store.user.spotifyUser?.images?.[0].url"
+            class="w-14 h-14 rounded-full"
+            alt="profile image"
+          >
+            <q-menu
+              class="shadow-sm whitespace-nowrap"
+              anchor="bottom middle"
+              self="top middle"
+              :offset="[5, 5]"
+              ><q-item clickable @click="onLogout">Sign out</q-item></q-menu
+            >
+          </q-img>
+        </button>
       </div>
     </q-header>
 
--- tubo/src/stores/user.ts
+++ tubo/src/stores/user.ts
@@ -1,13 +1,25 @@
 import type { SpotifyUser, User } from '@/schema'
 import { defineStore } from 'pinia'
+import { Cookies } from 'quasar'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export const useUserStore = defineStore('user', () => {
+  const router = useRouter()
+
   const spotifyUser = ref<SpotifyUser | null>(null)
   const tuboUser = ref<User | null>(null)
 
+  function logout() {
+    Cookies.remove('access_token')
+    Cookies.remove('user_id')
+
+    router.push('/')
+  }
+
   return {
     spotifyUser,
-    tuboUser
+    tuboUser,
+    logout
   }
 })
