52f2c8fb9ec338d946fd54c20b797b69870d0e6c
Lefan <lefantan@hotmail.com>
2023-07-05 12:18:28 -0600

    Fix login logo

--- tubo/src/views/LoginView.vue
+++ tubo/src/views/LoginView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import spotify from '@/assets/icons/spotify.png'
-
 const loginLink = `${import.meta.env.VITE_WEBAPI_URL}/rest/auth/login`
 </script>
 
 <template>
   <q-page class="flex items-center justify-center">
     <a :href="loginLink" class="btn text-2xl p-4 flex gap-2 items-center">
-      Connect to Spotify <img :src="spotify" class="w-10 h-10" />
+      Connect to Spotify <img src="/spotify.png" class="w-10 h-10" />
     </a>
   </q-page>
 </template>
