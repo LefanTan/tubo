99e8bcc3602de8411f6880caaec118715d132f5b
Lefan <lefantan@hotmail.com>
2024-06-03 11:35:08 -0600

    Update again

--- cloud/functions/warmer/index.js
+++ cloud/functions/warmer/index.js
@@ -2,9 +2,7 @@ import fetch from "node-fetch";
 import functions from "@google-cloud/functions-framework";
 
 functions.http("warmer", async (req, res) => {
-  await fetch("https://tubo-webapi.web.app/rest/").catch((err) =>
-    res.send(err)
-  );
+  await fetch("https://tubo-webapi-9ba99/rest/").catch((err) => res.send(err));
 
   res.send("Success!");
 });
