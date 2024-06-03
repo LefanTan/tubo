57cda93fd9b96c014a91dc122c65b20c71de9cad
Lefan <lefantan@hotmail.com>
2024-06-03 11:28:10 -0600

    Update webapi domain

--- cloud/functions/warmer/index.js
+++ cloud/functions/warmer/index.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import functions from "@google-cloud/functions-framework";
 
 functions.http("warmer", async (req, res) => {
-  await fetch("https://tubo-yludnsqgyq-uc.a.run.app/rest/").catch((err) =>
+  await fetch("https://tubo-webapi.web.app/rest/").catch((err) =>
     res.send(err)
   );
 
